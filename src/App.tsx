a2f3c104440061936675f6a22532781d0460d416
reipriantama <priantamareinato@gmail.com>
2024-06-07 18:59:31 +0700

    add video background

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import HeroSecond from "./components/feature/heroSecond";
 import Marque from "./components/feature/marque";
 import OurPartners from "./components/feature/ourPartners";
 import OurPraEvent from "./components/feature/ourPraEvent";
+import VideoSection from "./components/feature/videoSection";
 import Layout from "./components/layout";
 
 function App() {
   return (
-    <Layout className="bg-[#F2F4F7] ">
+    <Layout className="bg-[#F2F4F7]">
       <div>asdasd</div>
+      <VideoSection />
       <Marque />
       <OurPraEvent />
       <HeroSecond />
